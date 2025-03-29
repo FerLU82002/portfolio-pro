406117c1e798764442d0539f1a6eeb77994696ec
FERCHAS-DEV <luis.estela@unheval.pe>
2025-03-28 20:34:55 -0500

    Configuración Vite para github Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,12 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
+  base: '/portfolio-pro/', // 🔥 
   plugins: [tailwindcss()],
+  build: {
+    outDir: 'dist',       // 📦 
+    emptyOutDir: true,    // 🧹 
+    sourcemap: true       // 🗺️ 
+  }
 });
